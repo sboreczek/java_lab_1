47df1a6eba9ec69d9da59c130246e5e20a7f28a3
sboreczek <szymih2@gmail.com>
2021-12-18 08:14:57 +0100

    Zadanie 6 Tablica

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void wiek() {
-        int wiek = 21;
-        wiek = wiek % 3;
-        if(wiek == 0)
-            System.out.println("Podzielny przez 3");
-        else if(wiek != 0)
-            System.out.println("Niepodzielny przez 3");
+    public static void tablica() {
+        double[] tab = new double[5];
+        for(int i = 0;i < 5;i++)
+        {
+            tab[i] = i+ 2 + i * 13;
+        }
+        for(int i = 0;i < 5;i++)
+        {
+            System.out.println(tab[i]);
+        }
     }
-
-    public static void parzystosc() {
-        int indeks = 12;
-        String parzystosc = (indeks % 2 == 0) ? "indeks jest parzysty" : "indeks jest nieparzysty" ;
-        System.out.println(parzystosc);
-
-    }
-
-    public static void liczba(){
-        Scanner klawiatur = new Scanner(System.in);
-        float liczba = klawiatur.nextFloat();
-        if(liczba > 50)
-            System.out.println("Podana liczba jest większa od 50");
-        else if(liczba == 50)
-            System.out.println("Podana liczba to 50");
-        else if(liczba < 50)
-            System.out.println("Podana liczba jest mniejsza od 50");
-    }
-
 
     public static void main(String[] args) {
-        wiek();
-        parzystosc();
-        liczba();
+        tablica();
     }
 }
+
+
+
 
 
