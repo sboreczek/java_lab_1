cd5361a961acf5269220b81233db91637ac03bbd
sboreczek <szymih2@gmail.com>
2021-12-18 02:48:29 +0100

    Cwiczenie Trzecie

--- src/Main.java
+++ src/Main.java
@@ -35,16 +35,4 @@ public class Main {
     }
 }
 
-/* ćw.3 */
-
-/*  zad.1 napisać interfejs który bedzie słuzył do wpisania danych osobowych:
- *   a) Imię, nazwisko, wiek, nr indeksu,
- *   dane mają być wprowadzane z klawiatury w konsoli
- *   b) wyświetlić dane za pomocą println i printf
- */
-
-
-/*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
- *   wynik wyświetlić w konsoli programu
- */
 
